401d90b2a580b4184beadfa61437289b6867d2e4
schlitzered <stephan.schultchen@gmail.com>
2019-04-18 14:40:12 -0400

    move from swagger to openapi

--- dlm_engine/schemes/__init__.py
+++ dlm_engine/schemes/__init__.py
@@ -1,19 +1,19 @@
 import os
 
-from yaml import load
+from yaml import load, Loader
 
 with open('{0}/../static/swagger.yaml'.format(os.path.dirname(__file__)), 'r') as scheme_source:
-    schemes = load(scheme_source)
+    schemes = load(scheme_source, Loader=Loader)
 
-AUTHENTICATE_CREATE = schemes['definitions']['Authenticate_POST']
+AUTHENTICATE_CREATE = schemes['components']['schemas']['Authenticate_POST']
 
-CREDENTIALS_CREATE = schemes['definitions']['Common_Credential_POST']
+CREDENTIALS_CREATE = schemes['components']['schemas']['Common_Credential_POST']
 
-LOCKS_DELETE = schemes['definitions']['Lock_DELETE']
-LOCKS_CREATE = schemes['definitions']['Lock_POST']
+LOCKS_DELETE = schemes['components']['schemas']['Lock_DELETE']
+LOCKS_CREATE = schemes['components']['schemas']['Lock_POST']
 
-PERMISSIONS_CREATE = schemes['definitions']['Permission_POST']
-PERMISSIONS_UPDATE = schemes['definitions']['Permission_PUT']
+PERMISSIONS_CREATE = schemes['components']['schemas']['Permission_POST']
+PERMISSIONS_UPDATE = schemes['components']['schemas']['Permission_PUT']
 
-USERS_CREATE = schemes['definitions']['User_POST']
-USERS_UPDATE = schemes['definitions']['User_PUT']
+USERS_CREATE = schemes['components']['schemas']['User_POST']
+USERS_UPDATE = schemes['components']['schemas']['User_PUT']
